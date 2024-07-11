cc89e74c1be947c57b36ec86d61ed414dba95180
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-07-11 15:09:32 -0500

    Completed the side-bar for the personal website

--- personal-website/src/components/Sidebar/index.jsx
+++ personal-website/src/components/Sidebar/index.jsx
@@ -1,15 +1,37 @@
-import './index.scss'
-import { Link } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoZ from '../../assets/images/Z.png';
+import LogoSubtitle from '../../assets/images/Long-logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faSuitcase } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'; 
+import { faGithub,} from '@fortawesome/free-brands-svg-icons';
 
-
-const Sidebar = () =>  (
+const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
-            <img></img>
+            <img src={LogoZ} alt="logo" />
+            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
         </Link>
+        <nav>
+            <NavLink exact activeClassName="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink activeClassName="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink activeClassName="active" className="portfolio-link" to="/portfolio">
+                <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+            </NavLink>
+            <a className='LinkedIn-link' href="https://www.linkedin.com/in/z-taylor02" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+            <a className='Github-link' href="https://github.com/ZachTaylor2002" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+
+        </nav>
     </div>
+);
 
-)
-
-export default Sidebar
+export default Sidebar;
