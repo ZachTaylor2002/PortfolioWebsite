e066f12fb47d7cbf2ceccdd3b41e670234d1d3af
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-07-15 11:26:04 -0500

    About me section is completed

---
+++ personal-website/src/components/About/index.jsx
@@ -0,0 +1,49 @@
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => clearTimeout(timeout); // Cleanup function to clear the timeout
+    }, []);
+
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        idx={7} // Adjust idx to a valid index within strArray
+                    />
+                </h1>
+                <p>
+                    I'm a very ambitious front-end developer looking for a role in an
+                    established IT company with the opportunity to work with the latest
+                    technologies on challenging and diverse projects.
+                </p>
+
+                <p>
+                    I'm quiet confident, naturally curious, and perpetually working on
+                    improving my chops one design problem at a time.
+                </p>
+
+                <p>
+                    If I need to define myself in one sentence that would be a family
+                    person, father of a beautiful daughter, a sports fanatic,
+                    photography enthusiast, and tech-obsessed!!!
+                </p>
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default About;
