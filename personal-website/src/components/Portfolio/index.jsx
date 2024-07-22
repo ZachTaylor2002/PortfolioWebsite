3104b801d7079e07c7d83b82865e8bf62a791a32
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-07-22 10:11:10 -0500

    Added the "backbone" and ready to start the layout section

--- personal-website/src/components/Portfolio/index.jsx
+++ personal-website/src/components/Portfolio/index.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import "./index.scss";
+import React, {useState } from "react";
 import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
 
 const Portfolio = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
     return (
         <>
         <div className="container portfolio-page">
-            <h1 className="page-title">Portfolio</h1>
+            <h1 className="page-title">
+                <AnimatedLetters
+                 letterClass={letterClass}
+                 strArray={"Portfolio".split("")}
+                 idx={15}
+                />
+            </h1>
         </div>
         <Loader type="pacman" />
         </>
