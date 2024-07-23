b22bfb8d3cac598a4b15bc00c99e364eb1ae62a9
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-07-22 19:43:13 -0500

    Updated the Portfolio component to showcase the projects

--- personal-website/src/components/Portfolio/index.jsx
+++ personal-website/src/components/Portfolio/index.jsx
@@ -7,7 +7,6 @@ import portfolioData from "../../data/portfolio.json";
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
 
-    
     useEffect(() => {
         const timer = setTimeout(() => {
             setLetterClass('text-animate-hover');
@@ -17,6 +16,36 @@ const Portfolio = () => {
             clearTimeout(timer);
         }
     });
+
+    const renderPortfolio = (portfolio) => {
+        if (!portfolio.length) return <p>No projects available.</p>;
+    
+        return (
+            <div className="images-container">
+                {
+                    portfolio.map((port, idx) => (
+                        <div className="image-box" key={idx}>
+                            <img 
+                                src={port.cover} // Use "cover" instead of "image"
+                                className="portfolio-image"
+                                alt={port.title} // Updated alt text for accessibility
+                            />
+                            <div className="content">
+                                <p className="title">{port.title}</p> 
+                                <h4 className="description">{port.description}</h4>
+                                <button
+                                    className="btn"
+                                    onClick={() => window.open(port.url)}
+                                >
+                                    View
+                                </button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
 
 
     return (
@@ -29,7 +58,7 @@ const Portfolio = () => {
                  idx={15}
                 />
             </h1>
-            <div>{}</div>
+            <div>{renderPortfolio(portfolioData.portfolio)}</div>
         </div>
         <Loader type="pacman" />
         </>
